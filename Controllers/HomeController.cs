6c5f755c9841d81228f6afdaa4eb48c65bdd874c
Admin <19101001-154@uskt.edu.pk>
2022-07-02 14:36:31 +0500

    Quiz Form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,5 +8,14 @@ namespace Zaigham.Controllers
         {
             return View();
         }
+        public IActionResult quiz()
+        {
+            return View();
+        }
+        public IActionResult data(string name, string lname, string emaill, string gender, string num, string department)
+        {
+            ViewBag.Message = name + " " + lname + " has email: " + emaill + " and phone number: " + num + " and is a " + gender + " from " + department;
+            return View();
+        }
     }
 }
